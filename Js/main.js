0c8396e34385193129d1a50a69e30f5a0510a394
Chí Hưởng <>
2023-08-05 15:01:18 +0700

    banner-cate

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,18 @@
 window.onscroll = function() {scrollFunction()};
+var listNav = document.querySelectorAll(".subnav-list");
 // edit bgcolor of header
 function scrollFunction() {
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-    document.querySelector(".header").style.backgroundColor =  "#000";
+    let arrLength = listNav.length;
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector(".header").style.backgroundColor =  "#000"; 
+    for(var i = 0; i < arrLength; i++) {
+            listNav[i].style.backgroundColor =  "#000";
+        }
     } else {
     document.querySelector(".header").style.backgroundColor = "transparent";
+    for(var i = 0; i < arrLength; i++) {
+        listNav[i].style.backgroundColor =  "transparent";
+    }
     }
 }
 
