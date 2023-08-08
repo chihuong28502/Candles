2032f58f9a3a81bbcd3d7400795311a541772799
Chí Hưởng <>
2023-08-08 18:54:01 +0700

    slider product

--- Js/lirbary.js
+++ Js/lirbary.js
@@ -48,3 +48,43 @@ $(document).ready(function(){
           ]
     });
 });
+
+$(document).ready(function(){
+  $('.slide-product-list').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      vertical: true,
+      verticalSwiping: true,
+      prevArrow:"<button  type='button' class='icon-slider-product-up slick-prev pull-left'><i class='fa-solid fa-chevron-up' aria-hidden='true'></i></button>",
+      nextArrow:"<button  type='button' class='icon-slider-product-down slick-next pull-right'><i class='fa-solid fa-angle-down' aria-hidden='true'></i></button>"
+      // responsive: [
+      //     {
+      //       breakpoint: 1024,
+      //       settings: {
+      //         slidesToShow: 3,
+      //         slidesToScroll: 3,
+      //         infinite: true,
+      //         dots: true
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 600,
+      //       settings: {
+      //         slidesToShow: 2,
+      //         slidesToScroll: 2
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 480,
+      //       settings: {
+      //         slidesToShow: 1,
+      //         slidesToScroll: 1
+      //       }
+      //     }
+      //     // You can unslick at a given breakpoint now by adding:
+      //     // settings: "unslick"
+      //     // instead of a settings object
+      //   ]
+  });
+});
--- Js/main.js
+++ Js/main.js
@@ -49,5 +49,158 @@ var productBack = document.querySelectorAll(".product-back");
         }
     }
     clickTarget()
+    
 
 
+    // render products list
+//     var listProducts = [
+//         {
+//             id : 1,
+//             name :"Candles Spray Tshift",
+//             ImageFront: "../img/product-front1.jpeg",
+//             ImageBackend: "../img/product-backend1.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 2,
+//             name :"Candles Around Tshirt",
+//             ImageFront: "../img/product-front2.jpeg",
+//             ImageBackend: "../img/product-backend2.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 3,
+//             name :"Candles Roulette Short",
+//             ImageFront: "../img/product-front3.jpeg",
+//             ImageBackend: "../img/product-backend3.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 4,
+//             name :"Candles Spray Tshift",
+//             ImageFront: "../img/product-front1.jpeg",
+//             ImageBackend: "../img/product-backend1.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 5,
+//             name :"Candles Around Tshirt",
+//             ImageFront: "../img/product-front2.jpeg",
+//             ImageBackend: "../img/product-backend2.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 6,
+//             name :"Candles Roulette Short",
+//             ImageFront: "../img/product-front3.jpeg",
+//             ImageBackend: "../img/product-backend3.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 7,
+//             name :"Candles Spray Tshift",
+//             ImageFront: "../img/product-front1.jpeg",
+//             ImageBackend: "../img/product-backend1.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 8,
+//             name :"Candles Around Tshirt",
+//             ImageFront: "../img/product-front2.jpeg",
+//             ImageBackend: "../img/product-backend2.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 9,
+//             name :"Candles Roulette Short",
+//             ImageFront: "../img/product-front3.jpeg",
+//             ImageBackend: "../img/product-backend3.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 10,
+//             name :"Candles Spray Tshift",
+//             ImageFront: "../img/product-front1.jpeg",
+//             ImageBackend: "../img/product-backend1.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 11,
+//             name :"Candles Around Tshirt",
+//             ImageFront: "../img/product-front2.jpeg",
+//             ImageBackend: "../img/product-backend2.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 12,
+//             name :"Candles Roulette Short",
+//             ImageFront: "../img/product-front3.jpeg",
+//             ImageBackend: "../img/product-backend3.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 13,
+//             name :"Candles Spray Tshift",
+//             ImageFront: "../img/product-front1.jpeg",
+//             ImageBackend: "../img/product-backend1.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 14,
+//             name :"Candles Around Tshirt",
+//             ImageFront: "../img/product-front2.jpeg",
+//             ImageBackend: "../img/product-backend2.jpeg",
+//             price: 390000
+//         },
+//         {
+//             id : 15,
+//             name :"Candles Roulette Short",
+//             ImageFront: "../img/product-front3.jpeg",
+//             ImageBackend: "../img/product-backend3.jpeg",
+//             price: 390000
+//         }
+//     ];
+    
+//     function setupHtml(arr){
+//         var view = `
+//         <div class="list-product-item col-3">
+//             <div class="m-5 col product-item">
+//                 <div class="product-img">
+//                     <img class="w-100 product-front" src="${arr.ImageFront}" alt="">
+//                     <img class="w-100 product-back d-none" src="${arr.ImageBackend}" alt="">
+//                 </div>
+//                 <div class="product-description">
+//                     <h3>${arr.name}</h3>
+//                     <p><span>${arr.price}</span></p>
+//                 </div>
+//             </div>
+//             <div class="m-5 col product-item">
+//                 <div class="product-img">
+//                     <img class="w-100 product-front" src="${arr.ImageFront}" alt="">
+//                     <img class="w-100 product-back d-none" src="${arr.ImageBackend}" alt="">
+//                 </div>
+//                 <div class="product-description">
+//                     <h3>${arr.name}</h3>
+//                     <p><span>${arr.price}</span></p>
+//                 </div>
+//             </div>
+//             <div class="m-5 col product-item">
+//                 <div class="product-img">
+//                     <img class="w-100 product-front" src="${arr.ImageFront}" alt="">
+//                     <img class="w-100 product-back d-none" src="${arr.ImageBackend}" alt="">
+//                 </div>
+//                 <div class="product-description">
+//                     <h3>${arr.name}</h3>
+//                     <p><span>${arr.price}</span></p>
+//                 </div>
+//             </div>
+//         </div>
+//         `
+//         $('.product-list').append(view)
+//     }
+//     function renderHtml(){
+//         for(var i = 0; i <listProducts.length; i++){
+//             setupHtml(listProducts[i])
+//         }
+//     }
+// renderHtml()
